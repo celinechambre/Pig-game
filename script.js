d4dc48080f24067f0d13d80134ebba27b4ff75b5
Celine Chambre <chambre.celine@gmail.com>
2020-10-27 15:59:53 +0100

    switch active player

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -14,7 +16,10 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling the dice
 btnRoll.addEventListener('click', function() {
@@ -24,8 +29,12 @@ btnRoll.addEventListener('click', function() {
 
 	if(dice !== 1) {
 		currentScore += dice;
-		current0El.textContent = currentScore;
+		document.getElementById(`current--${activePlayer}`).textContent = currentScore;
 	} else {
-
+		document.getElementById(`current--${activePlayer}`).textContent = 0;
+		currentScore = 0;
+		activePlayer = activePlayer === 0 ? 1 : 0;
+		player0El.classList.toggle('player--active');
+		player1El.classList.toggle('player--active');
 	}
 })
